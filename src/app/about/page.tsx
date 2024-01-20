aa81641eb30d79897e4af3ba0705ad3d6052aeb2
SergeP <serge.prysiazhnyi@gmail.com>
2024-01-20 18:52:55 +0200

    basic routes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-blue-500">About</h1>
+      <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+    </div>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Sorry, this page does not exist</p>
+    </div>
+  );
+}
