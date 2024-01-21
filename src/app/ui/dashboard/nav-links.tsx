0997e965555376cfddac028ba4313cedfe632560
SergeP <serge.prysiazhnyi@gmail.com>
2024-01-21 14:06:26 +0200

    active link styles

--- src/app/ui/dashboard/nav-links.tsx
+++ src/app/ui/dashboard/nav-links.tsx
@@ -1,11 +1,15 @@
+"use client";
+
+import clsx from "clsx";
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
+  ArchiveBoxIcon,
 } from "@heroicons/react/24/outline";
 
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
+import { usePathname } from "next/navigation";
+
 const links = [
   { name: "Home", href: "/dashboard", icon: HomeIcon },
   {
@@ -14,9 +18,16 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: "Customers", href: "/dashboard/customers", icon: UserGroupIcon },
+  {
+    name: "About",
+    href: "/about",
+    icon: ArchiveBoxIcon,
+  },
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
@@ -25,7 +36,12 @@ export default function NavLinks() {
           <a
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+              {
+                "bg-sky-100 text-blue-600": pathname === link.href,
+              }
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
